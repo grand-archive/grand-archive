e3eed20fa627c9b59833dd39e19d794af4c7b22b
Max Wo <hello@maxwowo.com>
2023-11-27 23:02:16 +1300

    feat: use next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,14 @@ import '@/styles/globals.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider>
+    <DefaultSeo
+      description="Play Grand Archive online!"
+      title="Grand Archive"
+    />
     <Component {...pageProps} />
   </ChakraProvider>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,63 @@
 import { Box, Center, Heading, HStack, Text, VStack } from '@chakra-ui/react'
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Home = () => {
   return (
-    <>
-      <Head>
-        <title>Grand Archive</title>
-        <meta content="Play Grand Archive online!" name="description" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link href="/favicon.ico" rel="icon" />
-      </Head>
-      <Box
-        bgImage="url('/background-patterns.jpg')"
-        bgRepeat="repeat"
-        h="100%"
-        w="100%"
-      >
-        <VStack h="100%">
-          <Box
-            bg="black"
-            bgImage="url('/navbar-border.png')"
-            bgPosition="bottom"
-            bgRepeat="repeat-x"
-            bgSize="2px 6px"
-            pb="3"
-            pt="2"
-            px="6"
-            w="100%"
-          >
-            <HStack justify="space-between" maxW="6xl" mx="auto">
-              <Link href="/">
-                <Box w="24">
-                  <Image
-                    alt="Logo"
-                    height={116}
-                    src="/logo-navbar.png"
-                    style={{ width: '100%', height: 'auto' }}
-                    width={300}
-                  />
-                </Box>
-              </Link>
-            </HStack>
-          </Box>
+    <Box
+      bgImage="url('/background-patterns.jpg')"
+      bgRepeat="repeat"
+      h="100%"
+      w="100%"
+    >
+      <VStack h="100%">
+        <Box
+          bg="black"
+          bgImage="url('/navbar-border.png')"
+          bgPosition="bottom"
+          bgRepeat="repeat-x"
+          bgSize="2px 6px"
+          pb="3"
+          pt="2"
+          px="6"
+          w="100%"
+        >
+          <HStack justify="space-between" maxW="6xl" mx="auto">
+            <Link href="/">
+              <Box w="24">
+                <Image
+                  alt="Logo"
+                  height={116}
+                  src="/logo-navbar.png"
+                  style={{ width: '100%', height: 'auto' }}
+                  width={300}
+                />
+              </Box>
+            </Link>
+          </HStack>
+        </Box>
 
-          <Center flex="1">
-            <Heading textColor="white">Bruh</Heading>
-          </Center>
+        <Center flex="1">
+          <Heading textColor="white">Bruh</Heading>
+        </Center>
 
-          <Box
-            bg="black"
-            bgImage="url('/navbar-border.png')"
-            bgPosition="top"
-            bgRepeat="repeat-x"
-            bgSize="2px 6px"
-            pb="3"
-            pt="4"
-            px="6"
-            w="100%"
-          >
-            <HStack justify="space-between" maxW="6xl" mx="auto">
-              <Text textColor="white">Made by Max</Text>
-            </HStack>
-          </Box>
-        </VStack>
-      </Box>
-    </>
+        <Box
+          bg="black"
+          bgImage="url('/navbar-border.png')"
+          bgPosition="top"
+          bgRepeat="repeat-x"
+          bgSize="2px 6px"
+          pb="3"
+          pt="4"
+          px="6"
+          w="100%"
+        >
+          <HStack justify="space-between" maxW="6xl" mx="auto">
+            <Text textColor="white">Made by Max</Text>
+          </HStack>
+        </Box>
+      </VStack>
+    </Box>
   )
 }
 
