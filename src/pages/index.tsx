55760e31929115279894d0866d332ff03d1e371a
Max Wo <hello@maxwowo.com>
2023-11-27 18:55:15 +1300

    feat: project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Grand Archive</title>
+        <meta name="description" content="Play Grand Archive online!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Bruh</main>
+    </>
+  );
+};
+
+export default Home;
