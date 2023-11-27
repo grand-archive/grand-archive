44cba31f27466190dd70478659ff87b5e4f507e7
Max Wo <hello@maxwowo.com>
2023-11-27 20:11:39 +1300

    refactor: firebase client

--- src/clients/firebase.ts
+++ src/clients/firebase.ts
@@ -1,15 +1,14 @@
-import { getApps, initializeApp } from 'firebase/app'
+import { getApp, getApps, initializeApp } from 'firebase/app'
 
-export const firebase =
-  getApps().length === 0
-    ? initializeApp({
-        apiKey: 'AIzaSyASRWGh4R3nuEphAkuEdOrjtEcmhiWk1FE',
-        authDomain: 'grand-archive.firebaseapp.com',
-        databaseURL: 'https://grand-archive-default-rtdb.firebaseio.com',
-        projectId: 'grand-archive',
-        storageBucket: 'grand-archive.appspot.com',
-        messagingSenderId: '703520202408',
-        appId: '1:703520202408:web:81373546a13c4eacb2ec9d',
-        measurementId: 'G-8YNSXL34Q7'
-      })
-    : getApps()[0]
+export const firebase = !getApps().length
+  ? initializeApp({
+      apiKey: 'AIzaSyASRWGh4R3nuEphAkuEdOrjtEcmhiWk1FE',
+      authDomain: 'grand-archive.firebaseapp.com',
+      databaseURL: 'https://grand-archive-default-rtdb.firebaseio.com',
+      projectId: 'grand-archive',
+      storageBucket: 'grand-archive.appspot.com',
+      messagingSenderId: '703520202408',
+      appId: '1:703520202408:web:81373546a13c4eacb2ec9d',
+      measurementId: 'G-8YNSXL34Q7'
+    })
+  : getApp()
